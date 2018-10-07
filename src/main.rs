dbe6557e922d87b603774bb06223d4844ffa35ba
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2018-10-06 21:02:20 -0300

    Added --in, --out and --factor command line arguments

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,54 @@ use image::{GenericImage, GenericImageView, ImageBuffer};
 
 type rgba = image::Rgba<u8>;
 
-fn process_pixel(pixel: &rgba,factor:f64) -> rgba {
+fn process_pixel(pixel: &rgba, factor: u8) -> rgba {
     let (r, g, b) = (pixel[0], pixel[1], pixel[2]);
+    let factor:f64 = factor as f64;
 
     let r = ((factor * (r as f64) / 255f64).round() * (255f64 / factor)) as u8;
     let g = ((factor * (g as f64) / 255f64).round() * (255f64 / factor)) as u8;
     let b = ((factor * (b as f64) / 255f64).round() * (255f64 / factor)) as u8;
-    
+
     let out = image::Rgba([r, g, b, 0xFFu8]);
     out
 }
 fn main() {
-    let file_name = "picture.jpg";
-    let img = image::open(&file_name).expect("Couldn't open picture.jpg");
+    let mut input_file_name = String::from("picture.jpg");
+    let mut output_file_name = String::from("output.png");
+    let mut factor = 4u8;
+
+    let mut args_list: Vec<_> = std::env::args().rev().collect();
+    args_list.pop();
+    while args_list.len() > 0 {
+        let arg: String = args_list.pop().unwrap();
+
+        match arg.as_ref() {
+            "--in" => {
+                input_file_name = args_list.pop().expect("No more arguments passed");
+            }
+            "--out" => {
+                output_file_name = args_list.pop().expect("No more arguments passed");
+            }
+            "--factor" => {
+                factor = args_list.pop().expect("No more arguments passed").parse::<u8>().expect("Couldn't parse input for --factor. It must be an u8");
+            }
+            &_ => {
+                println!("Meaningless {}", arg);
+            }
+        }
+    }
+
+    let img = image::open(&input_file_name).expect("Couldn't open picture.jpg");
     let (width, height) = img.dimensions();
     let mut out = image::RgbaImage::new(width, height);
 
-    let args:Vec<_> = std::env::args().collect();
-    
-    
-    println!("{} is {}x{}", &file_name, &width, &height);
-    println!("Starting filtering...");
+    println!("{} is {}x{}", &input_file_name, &width, &height);
+    println!("Starting filtering with {} as factor...",factor);
 
     for y in 1..(height - 1) {
         for x in 1..(width - 1) {
             let old_pixel = img.get_pixel(x, y);
-            let new_pixel = process_pixel(&old_pixel,16f64);
+            let new_pixel = process_pixel(&old_pixel, factor);
             out.put_pixel(x, y, new_pixel);
 
             let mut loss_diference: [i16; 3] = [
@@ -46,15 +68,16 @@ fn main() {
                 let next_pixel = img.get_pixel(this_x, this_y);
                 let mut modified_pixel: [u8; 4] = [0x00, 0x00, 0x00, 0xFFu8];
                 for i in 0..2 {
-                    modified_pixel[i] =
-                        ((next_pixel[i] as f64) + ((loss_diference[i] as f64) * (quant[i] / 16f64))) as u8;
+                    modified_pixel[i] = ((next_pixel[i] as f64)
+                        + ((loss_diference[i] as f64) * (quant[i] / 16f64)))
+                        as u8;
                 }
                 out.put_pixel(this_x, this_y, image::Rgba(modified_pixel));
             }
         }
     }
 
-    println!("Saving...");
-    out.save("output.png").expect("Couldn't save output.png");
+    println!("Saving in {}...",output_file_name);
+    out.save(output_file_name).expect("Couldn't save output.png");
     println!("Saved");
 }
